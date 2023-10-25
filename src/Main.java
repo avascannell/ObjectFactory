61dbcd1107e9e5637e8f1e80b9a70d30547adc9f
avascannell <ava_scannell26@milton.edu>
2023-10-25 09:03:49 -0400

    create ponys setps 1 and 2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ponys p = new Ponys();
+        System.out.println(p.isUnicorn);
+        p.isUnicorn = true;
+        System.out.println(p.isUnicorn);
+    }
+}
---
+++ src/Ponys.java
@@ -0,0 +1,11 @@
+public class Ponys {
+    //true/false=boolean
+    //numbers= int  double= decimal
+    //words= String
+
+    boolean isUnicorn = false;
+    boolean isPegasus = false;
+    double size = 20.56;
+    String name = "rainbowdash";
+            String color = "blue";
+}
